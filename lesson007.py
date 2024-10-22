cd6531e62653fc113ce792812494df8681d8601e
Mark Peters <mapeters101@earthlink.net>
2024-10-22 17:23:21 -0400

    Lesson007: Complete work on Multi-Line Statements and Strings from 12:30.

--- lesson007.py
+++ lesson007.py
@@ -58,6 +58,73 @@ a = [1 # comment
 print(a)
 
 
+a = (1 #comment
+    ,2 #comment
+    ,3)
+print(a)
+
+a = {'key1': 1 #comment
+    ,'key2': 2 #comment
+    }
+# a = {'key1': 1 #comment
+#     ,'key2': 2 #comment }
+print(a)
+
+def my_func(a, #comment
+            b, #comment
+            c):
+    print(a, b, c)
+
+my_func(10, 20, 30)
+my_func(10, #comment
+        20, #comment
+        30 #comment
+        )
+
+a = 10
+b = 20
+c = 30
+
+if a > 5 and b > 10 and c > 20:
+    print('yes')
+
+if a > 5 \
+   and b > 10 \
+   and c > 20:
+    print('yes')
+
+a = '''this is a string'''
+print(a)
+
+a = '''this
+ is a string'''
+print(a)
+
+a = '''this
+    is a string
+        that is created over multiple lines'''
+print(a)
+
+a = '''some items:
+        1. item 1
+        2. item 2'''
+print(a)
+
+
+def my_func():
+    a = '''a multi-line string
+    that is indented in the second line'''
+    return a
+
+print(my_func())
+
+def my_func():
+    a = '''a multi-line string
+that is indented in the second line'''
+    return a
+
+print(my_func())
+
 
 
 
