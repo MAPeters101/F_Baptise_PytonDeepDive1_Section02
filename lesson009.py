7ccd4f9ff6d1bd2edd8b46164109456481cae073
Mark Peters <mapeters101@earthlink.net>
2024-10-22 17:45:47 -0400

    Lesson009: Work on Conditionals.

---
+++ lesson009.py
@@ -0,0 +1,40 @@
+a = 6
+
+if a < 5:
+    print('a < 5')
+else:
+    print('a >= 5')
+
+a = 10
+if a < 5:
+    print('a < 5')
+else:
+    if a < 10:
+        print('5 <= a < 10')
+    else:
+        print('a >= 10')
+
+a = 5
+if a < 5:
+    print('a < 5')
+elif a < 10:
+    print('5 <= a < 10')
+elif a < 15:
+    print('10 <= a < 15')
+elif a < 20:
+    print('15 <= a < 20')
+else:
+    print('a > 20')
+
+a = 25
+if a < 5:
+    b = 'a < 5'
+else:
+    b = 'a >= 5'
+print(b)
+
+b = 'a < 5' if a < 5 else 'a >= 5'
+print(b)
+
+a = 4
+print('a < 5' if a < 5 else 'a >= 5')
